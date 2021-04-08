e7cae9b1a0337f804bf2475b257c0aa4905e3a0b
nicolasds1 <nicodisalvo11@gmail.com>
2021-04-08 15:47:25 -0300

    Ejercicio#3

--- Ejercicios#3/main.py
+++ Ejercicios#3/main.py
@@ -6,19 +6,34 @@ class BasePorAltura:
 
     def calcBasePorAltura(self):
         res = self.base * self.altura
-        print(res)
+        print("Resultado Ejercicio 1:", res)
 
-finalResult = BasePorAltura(10, 5)
-finalResult.calcBasePorAltura()       
+resultadoEjercicio1 = BasePorAltura(10, 5)
+resultadoEjercicio1.calcBasePorAltura()       
 
 #Ejercico 2:
 class Rectangulo:
-    def : __init__ (self, base, altura):
+    def __init__ (self, base, altura):
         self.base = base 
         self.altura = altura
 
     def calcularBasePorAltura (self):
         resultado = self.base * self.altura
+        print("Resultado Ejercicio 2:", resultado)
 
-                    
-    
+resultadoEjercicio2 = Rectangulo(10, 9)
+resultadoEjercicio2.calcularBasePorAltura()                   
+
+#Ejercicio 3:
+class calcularAreaTrianguloRectangulo:
+    def __init__ (self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def calcAreaTrianguloRectangulo(self):
+        area = (self.base * self.altura) / 2
+        print("Resultado Ejercicio 3:", area)
+
+resultadoEjercicio3 = calcularAreaTrianguloRectangulo(7, 4)
+resultadoEjercicio3.calcAreaTrianguloRectangulo()
+
