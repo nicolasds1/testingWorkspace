532363178221e056cf167ad3eededae56d07b0e2
nicolasds1 <nicodisalvo11@gmail.com>
2021-04-08 15:32:29 -0300

    Subiendo Ejercicios Personalizados parte II

---
+++ Ejercicios#3/main.py
@@ -0,0 +1,24 @@
+#Ejercicio 1:
+class BasePorAltura:
+    def __init__ (self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def calcBasePorAltura(self):
+        res = self.base * self.altura
+        print(res)
+
+finalResult = BasePorAltura(10, 5)
+finalResult.calcBasePorAltura()       
+
+#Ejercico 2:
+class Rectangulo:
+    def : __init__ (self, base, altura):
+        self.base = base 
+        self.altura = altura
+
+    def calcularBasePorAltura (self):
+        resultado = self.base * self.altura
+
+                    
+    
